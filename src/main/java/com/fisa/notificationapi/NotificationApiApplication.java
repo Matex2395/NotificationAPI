4f74272e22d618ba31bc610a212a29eaf43dab29
Camila Sánchez <102784592+camilasanchezu@users.noreply.github.com>
2026-01-24 11:34:55 -0500

    Eureka Connection

--- src/main/java/com/fisa/notificationapi/NotificationApiApplication.java
+++ src/main/java/com/fisa/notificationapi/NotificationApiApplication.java
@@ -3,8 +3,10 @@ package com.fisa.notificationapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAsync
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationApiApplication {
 
